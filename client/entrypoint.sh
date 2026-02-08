d9506b5897306bdf1a2bea88fad6be16af615027
jonnasson <jona.kozik@gmail.com>
2026-02-08 23:37:00 +0100

    Update entrypoint.sh

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # Extract the DNS resolver from /etc/resolv.conf for nginx dynamic resolution
-export RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
+# Wrap IPv6 addresses in brackets as required by nginx resolver directive
+RAW=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
+case "$RAW" in
+  *:*) export RESOLVER="[$RAW]" ;;
+  *)   export RESOLVER="$RAW" ;;
+esac
 exec /docker-entrypoint.sh "$@"
